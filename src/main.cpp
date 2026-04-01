504c90b664529166e4c173cc4c8bc0c7bdcac5aa
NIYCCO <nico.rigsinger@hotmail.com>
2026-04-01 14:27:45 +0200

    add icon for sim

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <stdexcept>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include <GLFW/glfw3.h>
+
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
 int main() {
     if (!glfwInit()) return -1;
+
+    GLFWimage icons[2];
+    icons[0].pixels = stbi_load("assets/icon_16.png", &icons[0].width, &icons[0].height, nullptr, 4);
+    icons[1].pixels = stbi_load("assets/icon_32.png", &icons[1].width, &icons[1].height, nullptr, 4);
+
+    if (!icons[0].pixels || !icons[1].pixels) {
+        std::cerr << "Failed to load icons" << std::endl;
+        return -1;
+    }
     
     GLFWwindow* window = glfwCreateWindow(1280, 720, "PIC16F84 Simulator", nullptr, nullptr);
+    glfwSetWindowIcon(window, 2, icons);
+    
+    if (icons[0].pixels) stbi_image_free(icons[0].pixels);
+    if (icons[1].pixels) stbi_image_free(icons[1].pixels);
+
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
